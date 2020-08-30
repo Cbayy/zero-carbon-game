588fc72aece586c004895f4ad452e4088a2307cf
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-08-30 19:00:56 +0930

    Started adding grid generation function

--- zero-carbon-game/Assets/Scripts/GridManager.cs
+++ zero-carbon-game/Assets/Scripts/GridManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    int columns = 20;
+    int rows = 20;
+    float tileSize = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        generateGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void generateGrid(){
+        //GameObject referenceTile = (GameObject)Instantiate(Resources.load("tilename)); add tile sprite
+        for(int row = 0; row < rows; row++){
+            for(int column = 0; column < columns; column++){
+
+            }
+        }
     }
 }
