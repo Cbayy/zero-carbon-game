f4e255a710a4ca643e4c4d02ecb02d5c0d7e361b
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-08-31 22:15:33 +0930

    Added basic player mouse movement.

---
+++ zero-carbon-game/Assets/Scripts/playerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    float speed = 5;
+    Vector3 targetPosition;
+    bool isMoving = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            setTargetPosition();
+        }
+        if(isMoving){
+            move();
+        }
+    }
+
+    void setTargetPosition(){
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+        isMoving = true;
+    }
+
+    void move(){
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if(transform.position == targetPosition){
+            isMoving = false;
+        }
+    }
+
+}
