be1b79e12fcfc00f8e5044d1c315f826d508d944
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-03 23:54:01 +0930

    Added assets.

---
+++ zero-carbon-game/Assets/Scripts/TileClicked.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.TileMaps;
+
+public class TileClicked : MonoBehaviour
+{
+    /*
+    public TileMap tilemap;
+    public TileBase swappedTile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Vector3 pos = Input.mousePosition;
+            Vector3Int tilePos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(pos));
+            tileMap
+        }
+        
+    }
+    */
+}
