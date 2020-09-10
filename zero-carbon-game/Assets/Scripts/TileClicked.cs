2b5a878c7de452900188c82712be890ce94b8933
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-10 09:50:17 +0930

    Added function to return clicked grid position.

--- zero-carbon-game/Assets/Scripts/TileClicked.cs
+++ zero-carbon-game/Assets/Scripts/TileClicked.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 
 public class TileClicked : MonoBehaviour
 {
-    /*
-    public TileMap tilemap;
-    public TileBase swappedTile;
+
+    //public TileMap tilemap;
+    //public TileBase swappedTile;
     
+    public Grid grid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log("ru2n");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
+            /*
             Vector3 pos = Input.mousePosition;
             Vector3Int tilePos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(pos));
             tileMap
+            */
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            Debug.Log(position);
         }
         
     }
-    */
+
 }
