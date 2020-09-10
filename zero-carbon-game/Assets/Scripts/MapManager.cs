06584c1eebd870e18b1bb3d4b78c89167f257441
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-10 11:01:29 +0930

    Added components to detect the type of tile clicked.

---
+++ zero-carbon-game/Assets/Scripts/MapManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    private void Update(){
+        if(Input.GetMouseButton(0)){
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            print("At pos: " + gridPosition +  " there is a " + clickedTile);
+        }
+    }
+}
