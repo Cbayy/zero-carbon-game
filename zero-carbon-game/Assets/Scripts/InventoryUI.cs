84681309ee70ca3e734ff1129dcd176d5f431aa0
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-15 15:56:07 +0930

    Updated inventory functions.

---
+++ zero-carbon-game/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    //Inventory inventory;
+
+    void Start()
+    {
+        /*
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI(){
+        Debug.Log("Updating UI");
+    }
+}
---
+++ zero-carbon-game/Assets/exitInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class exitInventory : MonoBehaviour
+{
+
+    public Button button;
+    
+    public GameObject inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);   
+    }
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+        inventory.gameObject.SetActive(false);
+	}
+}
+
