cbb9c5a9d3b859dc6fcacc28d50599dfe1b91903
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-25 00:38:44 +0930

    Started adding code for placing items from inventory into grid.

--- zero-carbon-game/Assets/Scripts/useItem.cs
+++ zero-carbon-game/Assets/Scripts/useItem.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class useItem : MonoBehaviour
 {
 
     private Inventory inventory;
 
+    public Tilemap tilemap;
+
+    public Grid grid;
+
+    private bool placing = false;
+
+    int i = 1;
+
     private void Start(){
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        tilemap = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<Tilemap>();
+        grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
     }
     
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)){
-            print(this.name);
+            print("aa" + this.name);
+                placing = true;
         }
-
     }
+
+    void FixedUpdate(){
+            if(Input.GetMouseButtonDown(0) && placing == true){
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+                Vector3Int position = grid.WorldToCell(worldPoint);
+                Debug.Log(position);
+                placing = false;
+                //this.GameObject.destroy;
+            }
+    }
+
 }
