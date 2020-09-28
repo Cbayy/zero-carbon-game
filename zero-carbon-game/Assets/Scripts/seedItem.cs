c94f1956be878ede86153065a5a1e84668e1e583
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-28 23:42:18 +0930

    Placing a seed now removes only the instance selected.

--- zero-carbon-game/Assets/Scripts/seedItem.cs
+++ zero-carbon-game/Assets/Scripts/seedItem.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class seedItem : MonoBehaviour
 {
-        private Inventory inventory;
+    private Inventory inventory;
     public Tilemap tilemap;
 
     public Grid grid;
@@ -14,34 +15,79 @@ public class seedItem : MonoBehaviour
 
     public TileBase grassSprite;
 
+    public seedItem button;
+
+    //new private string name;
+
     // Start is called before the first frame update
     void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
         tilemap = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<Tilemap>();
         grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
+        //button = GameObject.FindGameObjectWithTag("Slot").GetComponent<Image>();
+        //gameObject.name = GetInstanceID().ToString();
+        //name = gameObject.name;
     }
 
+
+    public void Use(){
+        StartCoroutine("WaitForAnswer");
+    }
+
+
+    public IEnumerator WaitForAnswer()
+    {
+        bool wait = true;
+        while(wait){
+            if (Input.GetMouseButtonDown(0))
+            {
+                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+                        Vector3Int position = grid.WorldToCell(worldPoint);
+
+                        placing = false;
+                        tilemap.SetTile(position, grassSprite);
+
+
+                        wait = false;
+                        Destroy(gameObject);
+                        
+            }
+            yield return null;
+        }
+    }        
+
+/*
+
+    void Update(){
+        if(Input.GetMouseButtonDown(0) && placing == true){
+                    //button = GameObject.FindGameObjectWithTag("Slot").GetComponent<Image>();
+                    button = this;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+                    Vector3Int position = grid.WorldToCell(worldPoint);
+
+                    placing = false;
+                    tilemap.SetTile(position, grassSprite);
+                    print("NAME:  " + gameObject.name);
+                    Destroy(GameObject.Find(name));
+                    
+                }
+    }
+    */
+
+//CHANGE TO CLICKED
+/*
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)){
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2)){
             print("aa" + this.name);
-                    print("THIS: " + inventory.slots[0].name);
                 placing = true;
+                print("ITS: " + gameObject.name);
         }
 
     }
+*/
 
-    void FixedUpdate(){
-            if(Input.GetMouseButtonDown(0) && placing == true){
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
-                Vector3Int position = grid.WorldToCell(worldPoint);
-                Debug.Log(position);
-                print("YEET" + this.name);
-                placing = false;
-                tilemap.SetTile(position, grassSprite);
-                Destroy(gameObject);
-            }
-    }
 }
