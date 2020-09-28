29b3ffa22262de7391723391df5a823ab9c2baf1
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-09-28 23:43:00 +0930

    Previously full (now empty) inventory slots can be refilled.

---
+++ zero-carbon-game/Assets/Scripts/slotItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotItem : MonoBehaviour
+{
+    private Inventory inventory;
+    public int i;
+    
+    void Start() {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void Update(){
+        if(transform.childCount <= 0){
+            inventory.isFull[i] = false;
+        }
+    }
+}
