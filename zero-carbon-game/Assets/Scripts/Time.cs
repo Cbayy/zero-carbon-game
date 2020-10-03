d11d07c39e1a1a216b7efc3e8a81ff9684b86f61
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-10-03 12:59:13 +0930

    Added day counter interface and code.

---
+++ zero-carbon-game/Assets/Scripts/Time.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour
+{
+
+    public Text timeTxt;
+
+    int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        int result = (time % 10000 >= 5000 ? time + 10000 - time % 10000 : time - time % 10000)/10000;
+        
+        timeTxt.text = "Day: " + result.ToString();
+    }
+}
