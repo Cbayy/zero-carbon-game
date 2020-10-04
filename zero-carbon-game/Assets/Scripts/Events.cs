ad4b9bae35c9237853ea4e064ff335b18ff449d8
Cbayy <53809379+Cbayy@users.noreply.github.com>
2020-10-04 22:54:26 +1030

    Added script to process events.

---
+++ zero-carbon-game/Assets/Scripts/Events.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class Events : MonoBehaviour
+{
+
+    public Tilemap tilemap;
+    public Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tilemap = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<Tilemap>();
+        //grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
